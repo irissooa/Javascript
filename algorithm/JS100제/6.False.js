c8223f9ebaac0c6a4533d9a31f8685356bd74453
박수아 <xlalfskdl@naver.com>
2021-02-28 21:52:25 +0900

    [ADD]js study

---
+++ algorithm/JS100제/6.False.js
@@ -0,0 +1,11 @@
+// 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+// 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
+// 1)  NaN : False
+// 2)  1 : True
+// 3)  "" : False
+// 4)  0 : False
+// 5)  undefined : False
+
+// 2)  1
+
+// 정답은 '2번'입니다. JavaScript 에서는 null, undefined, 0, 빈 문자열, NaN, false 를 제외하고는 모두 참인 값으로 평가합니다.
