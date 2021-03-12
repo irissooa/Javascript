a9c3f1423e36a00d8d786375f87221bb782b3e5d
박수아 <xlalfskdl@naver.com>
2021-03-12 13:51:36 +0900

    [ADD] js study

---
+++ algorithm/JS100제/33.거꾸로출력하기.js
@@ -0,0 +1,13 @@
+/*
+한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
+*/
+
+const arr = prompt('숫자를 입력하세요',0).split(' ').reverse();
+const result = '';
+
+for (let i = 0; i < data.length; i++) {
+  result += data[i];
+}
+
+console.log(result);
+
