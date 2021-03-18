3db00020b2933658e89d37dfc8bdc1ed6babc135
박수아 <xlalfskdl@naver.com>
2021-03-19 02:36:37 +0900

    [ADD] js study includes

--- algorithm/JS100제/38.호준이의아르바이트.js
+++ algorithm/JS100제/38.호준이의아르바이트.js
@@ -4,6 +4,28 @@
 1위 ~ 3위 학생은 여러명일 수 있고 1~3위 학생 중 중복되는 학생까지 포함하여 사탕을 사기로 한다.
 학생들의 점수를 공백으로 구분하여 입력을 받고 사탕을 받을 학생의 수를 출력하세요.
 */
+
+const scores = prompt('점수를 입력하세요').split(' ').map((n) => {
+  return parseInt(n, 10);
+})
+
+scores.sort((a,b) => {
+  return a-b;
+});
+
+let cnt = 0;
+let arr = [];
+
+while (arr.length < 3) {
+  let n = scores.pop();
+  if (!arr.includes(n)) {
+    arr.push(n);
+  }
+  cnt += 1;
+}
+console.log(cnt);
+
+/*
 const scores = prompt('점수입력').split(' ').map(function (n) {
   return parseInt(n, 10);
 });
@@ -24,3 +46,4 @@ while (arr.length < 3) {
 }
 
 console.log(count);
+ */
