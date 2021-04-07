f39edb2c9337056b471f277c1ffdbb6dd8415399
박수아 <xlalfskdl@naver.com>
2021-04-08 03:23:05 +0900

    [ADD] js study bubblesort function

--- algorithm/JS100제/50.버블정렬구현하기.js
+++ algorithm/JS100제/50.버블정렬구현하기.js
@@ -1,13 +1,11 @@
 /**버블정렬은 두 인접한 원소를 검사하여 정렬하는 방법을 말합니다. 시간 복잡도는 느리지만 코드가 단순하기 때문에 자주 사용됩니다 
- * 
- 
-
 */
-function bubble(arr) {
+function bubbleSort(arr) {
   let result = arr.slice(); // 원본 배열 복사
 
   for (let i = 0; i < result.length - 1; i++) {
     for (let j = 0; j < result.length - i; j++) {
+      // 현재 원소가 다음 원소보다 크다면 자리 바꿈
       if (result[j] > result[j + 1]) {
         let temp = result[j];
         result[j] = result[j + 1];
